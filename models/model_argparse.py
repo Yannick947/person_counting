33e996b734bb81ee6014c2e8627b0bce4ab683b9
Yannick <yannick.schrempp@web.de>
2020-04-27 09:32:19 +0200

    Add arg lower cols

--- models/model_argparse.py
+++ models/model_argparse.py
@@ -10,8 +10,8 @@ def parse_args(args):
     parser.add_argument('--top-path',           help='The parent directory where csv feature and label files are stored', default='C:/Users/Yannick/Google Drive/person_detection/bus_videos/pcds_dataset_detected/')
     parser.add_argument('--label-file',         help='The name of the label file', default='pcds_dataset_labels_united.csv')
     parser.add_argument('--filter-rows-factor', help="The factor for filtering rows (3 indicates every third row will be left after filtering", default=2, type=int)
-    parser.add_argument('--filter-cols',        help='Removes this total amount of columns from the ends of every csv (5 will remove 10 cols)', default=5, type=int)
+    parser.add_argument('--filter-cols-upper',  help='Removes this total amount of columns from the upper boundary of the images', default=10, type=int)
+    parser.add_argument('--filter-cols-lower',  help='Removes this total amount of columns from the lower boundary of the images', default=25, type=int)
     parser.add_argument('--filter-cols-factor', help='After removing cols from the ends, filter the csv files with this factor (same as for rows)', default=1, type=int)
     parser.add_argument('--batch-size',         help='The batch size for training', default=16, type=int)
-    
     return parser.parse_args(args)
