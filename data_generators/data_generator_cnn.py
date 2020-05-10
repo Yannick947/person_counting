1fe4a8e23c620af4ae044a5e38fd5a9d3731775b
Yannick <yannick.schrempp@web.de>
2020-05-10 22:30:41 +0200

    refactor scaler, only use one

--- data_generators/data_generator_cnn.py
+++ data_generators/data_generator_cnn.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from person_counting.data_generators.data_generators import Generator_CSVS
 from person_counting.data_generators.data_generators import *
-
+from person_counting.utils.scaler import CSVScaler
 
 class Generator_CSVS_CNN(Generator_CSVS):
     '''
@@ -68,16 +68,17 @@ class Generator_CSVS_CNN(Generator_CSVS):
                 # Shape for x must be 4D [samples, timesteps, features, channels] and numpy array
                 if batch_index == self.batch_size:
                     batch_index = 0
-                    self.labels.extend(list(y_batch))
                     yield (x_batch, y_batch)
 
 
 def create_datagen(top_path, 
                    sample, 
                    label_file, 
-                   augmentation_factor=0): 
+                   augmentation_factor=0, 
+                   filter_hour_above=0, 
+                   filter_category_noisy=False): 
     '''
-    Creates train and test data generators for cnn network. 
+    Creates train and test data generators for lstm network. 
 
     Arguments: 
         top_path: Parent directory where shall be searched for training files
@@ -85,52 +86,40 @@ def create_datagen(top_path,
         label_file: Name of the label file containing all the labels
         augmentation_factor: Factor how much augmentation shall be done, 1 means
                              moving every pixel for one position
+        filter_hour_above: Hour after which videos shall be filtered
+        filter_category_noisy: Flag if noisy videos shall be filtered
     '''
-
+    #Load filenames and lengths
     length_t, length_y = get_filtered_lengths(top_path, sample)
-
     train_file_names, test_file_names = split_files(top_path, label_file)
-    print_train_test_lengths(train_file_names, test_file_names, top_path, label_file)
+
+    #Apply filters
+    train_file_names = apply_file_filters(train_file_names, filter_hour_above, filter_category_noisy)
+    test_file_names = apply_file_filters(test_file_names, filter_hour_above, filter_category_noisy)
+
+    print('Dataset contains: \n{} training csvs \n{} testing csvs'.format(len(train_file_names), len(test_file_names)))
+    
+    #TODO: Should be mix of train and test file names
+    scaler = CSVScaler(top_path, label_file, train_file_names, sample, sample_size=100)
 
     gen_train = Generator_CSVS_CNN(length_t,
                                    length_y,
                                    train_file_names,
+                                   scaler=scaler, 
                                    sample=sample,
                                    top_path=top_path,
                                    label_file=label_file, 
                                    augmentation_factor=augmentation_factor)
-    gen_train.create_scaler()
-
+    
     #Don't do augmentation here!
     gen_test = Generator_CSVS_CNN(length_t,
                                   length_y,
                                   test_file_names,
+                                  scaler=scaler, 
                                   sample=sample,
                                   top_path=top_path,
                                   label_file=label_file, 
                                   augmentation_factor=0)
-    gen_test.create_scaler()
 
     return gen_train, gen_test
 
-
-def main(args=None):
-
-    # parse arguments
-    if args is None:
-        args = sys.argv[1:]
-    args = parse_args(args)    
-    length_t, length_y = get_filtered_lengths(args.top_path, args.filter_rows_factor,
-                                              args.filter_cols_upper, args.filter_cols_lower)
-                                              
-    train_file_names, _ = split_files(args.top_path, args.label_file)
-
-    gen = Generator_CSVS_CNN(length_t, length_y,
-                         train_file_names, args.filter_cols_upper, args.filter_cols_lower,
-                         args.filter_rows_factor, batch_size=16)
-
-    for _ in range(5):
-        print(next(gen.datagen))
-
-if __name__ == '__main__':
-    main()
