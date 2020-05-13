18ef44ddbc58a34e6cb7fe47a7387b2f53b0dea4
Yannick <yannick.schrempp@web.de>
2020-05-13 15:49:33 +0200

    Fix logging

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -7,7 +7,10 @@ import os
 axs = list()
 vis_path = '/content/drive/My Drive/person_counting/visualizations/'
 
-def visualize_predictions(y_true, predictions, mode='val', ipython_mode=False):
+def visualize_predictions(y_true, predictions, mode='val', logdir=None):
+    '''Visualize predictions over ground truth
+    '''
+    #TODO: Implement colour for different categories (noisy, crowd/uncrowd)
     figure = plt.figure()
     plt.subplot(1,1,1)
     plt.scatter(predictions, y_true)
@@ -21,17 +24,14 @@ def visualize_predictions(y_true, predictions, mode='val', ipython_mode=False):
     plt.yticks(np.arange(min(np.append(predictions, y_true)),
                          max(np.append(predictions , y_true))))
 
-    if ipython_mode == False: 
-        plt.show()
-    else: 
-        plt.show()
+    if logdir is not None:
+        save_name = os.path.join(logdir, '{mode}_Pred_GT.png'.format(mode=mode))
+        figure.savefig(save_name)
+        print('Figure for mode {mode} saved in {save_name}'.format(mode=mode, save_name=save_name))
+    
+    plt.show()
 
-    save_name = vis_path + '{mode}_Pred_GT.png'.format(mode=mode)
-    figure.savefig(save_name)
-    print('Figure for mode {mode} saved in {save_name}'.format(mode=mode, save_name=save_name))
-
-
-def plot_losses(history, ipython_mode=False):
+def plot_losses(history, logdir=None):
     '''Plot the history of a model
     '''
     # plot history
@@ -42,37 +42,34 @@ def plot_losses(history, ipython_mode=False):
 
     plt.legend()
     plt.show()
-    save_name = vis_path + 'losses.png'
-    print(save_name)
-    figure.savefig(save_name)
-    print('Figure saved as losses.png')
-
-def visualize_filters(model):
-    for il, layer in enumerate(model.layers):
-        if 'conv' not in layer.name:
-            continue
-
-        # get filter weights
-        filters, _ = layer.get_weights()
-        print('layer_name: ', layer.name,', shape: ', filters.shape)
-        f_min, f_max = filters.min(), filters.max()
-        filters = (filters - f_min) / (f_max - f_min)
-        n_filters = len(filters)
-        ix = 1
-
-        figure = plt.figure()
-        for i in range(n_filters):
-            # get the filter
-            f = filters[:, :, :, i]
-            # specify subplot and turn of axis
-            ax = plt.subplot(n_filters, 1, ix)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            # plot filter channel in grayscale
-            plt.imshow(f[:, :, 0], cmap='gray')
-            figure.add_axes(ax)
-            ix += 1
-        
-        save_name = vis_path + 'filters_layer{}.png'.format(il)
+    if logdir is not None:
+        save_name = os.path.join(logdir, 'losses.png')
         figure.savefig(save_name)
-        plt.show()
+        print('Figure saved as losses.png')
+
+
+def visualize_filters(model, logdir=None):
+    if logdir is not None:
+        for il, layer in enumerate(model.layers):
+            if 'conv' not in layer.name:
+                continue
+
+            # get filter weights
+            filters, _ = layer.get_weights()
+            print('layer_name: ', layer.name,', shape: ', filters.shape)
+            f_min, f_max = filters.min(), filters.max()
+            filters = (filters - f_min) / (f_max - f_min)
+            n_filters = len(filters)
+            ix = 1
+
+            figure = plt.figure()
+            for i in range(n_filters):
+                f = filters[:, :, :, i]
+                ax = plt.subplot(n_filters, 1, ix)
+                plt.imshow(f[:, :, 0], cmap='gray')
+                figure.add_axes(ax)
+                ix += 1
+            
+            save_name = os.path.join(logdir, 'filters_layer{}.png'.format(il))
+            figure.savefig(save_name)
+            plt.show()
