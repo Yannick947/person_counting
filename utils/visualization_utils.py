b96e065e4b4729d5681e2ac59f0dff40efbea876
Yannick <yannick.schrempp@web.de>
2020-05-14 21:59:54 +0200

    Add violin plot

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -1,35 +1,58 @@
 
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 import os
 
-axs = list()
-vis_path = '/content/drive/My Drive/person_counting/visualizations/'
 
-def visualize_predictions(y_true, predictions, mode='val', logdir=None):
+def visualize_predictions(**kwargs):
     '''Visualize predictions over ground truth
     '''
+    scatterplot(**kwargs)
+    violinplot(**kwargs)
+
+
+def add_plot_attributes(method):
+    def plot_method(y_pred, y_true, mode, logdir):
+        figure = plt.figure()
+        plt.subplot(1,1,1)
+        method(y_pred, y_true, mode, logdir)
+        plt.title('Predictions over ground truth')
+        plt.xlabel('Ground truth')
+        plt.ylabel('Predictions')
+
+        plt.xticks(np.arange(min(np.append(y_pred, y_true)),
+                            max(np.append(y_pred , y_true))))
+
+        plt.yticks(np.arange(min(np.append(y_pred, y_true)),
+                            max(np.append(y_pred , y_true))))
+
+        if logdir is not None:
+            save_name = os.path.join(logdir, '{plottype}_{mode}_Pred_GT.png'.format(mode=mode,
+                                                                                    plottype=method.__name__))
+            figure.savefig(save_name)
+            print(method.__name__, ' created for mode {mode} and saved in {save_name}'.format(mode=mode,
+                                                                                              save_name=save_name))
+        plt.show()
+
+    return plot_method
+
+
+@add_plot_attributes
+def scatterplot(y_true, y_pred, mode, logdir):
+    '''Create a scatterplot for predictions
+    '''
     #TODO: Implement colour for different categories (noisy, crowd/uncrowd)
-    figure = plt.figure()
-    plt.subplot(1,1,1)
-    plt.scatter(predictions, y_true)
-    plt.title('Predictions over ground truth')
-    plt.xlabel('Predictions')
-    plt.ylabel('Ground truth')
+    plt.scatter(y_pred, y_true)
 
-    plt.xticks(np.arange(min(np.append(predictions, y_true)),
-                         max(np.append(predictions , y_true))))
 
-    plt.yticks(np.arange(min(np.append(predictions, y_true)),
-                         max(np.append(predictions , y_true))))
+@add_plot_attributes
+def violinplot(y_true, y_pred, mode, logdir):
+    ''' Create a violinplot for predictions
+    '''
+    sns.violinplot(x=y_pred, y=y_true) 
 
-    if logdir is not None:
-        save_name = os.path.join(logdir, '{mode}_Pred_GT.png'.format(mode=mode))
-        figure.savefig(save_name)
-        print('Figure for mode {mode} saved in {save_name}'.format(mode=mode, save_name=save_name))
-    
-    plt.show()
 
 def plot_losses(history, logdir=None):
     '''Plot the history of a model
@@ -50,6 +73,8 @@ def plot_losses(history, logdir=None):
 
 def visualize_filters(model, logdir=None):
     if logdir is not None:
+        print('\nSaving convolutional filters for vizualization ..')
+
         for il, layer in enumerate(model.layers):
             if 'conv' not in layer.name:
                 continue
@@ -59,16 +84,13 @@ def visualize_filters(model, logdir=None):
             print('layer_name: ', layer.name,', shape: ', filters.shape)
             f_min, f_max = filters.min(), filters.max()
             filters = (filters - f_min) / (f_max - f_min)
-            n_filters = len(filters)
-            ix = 1
-
             figure = plt.figure()
-            for i in range(n_filters):
+
+            for i in range(int(filters.shape[3])):
                 f = filters[:, :, :, i]
-                ax = plt.subplot(n_filters, 1, ix)
+                ax = plt.subplot(int(filters.shape[3]), 1, i + 1)
                 plt.imshow(f[:, :, 0], cmap='gray')
                 figure.add_axes(ax)
-                ix += 1
             
             save_name = os.path.join(logdir, 'filters_layer{}.png'.format(il))
             figure.savefig(save_name)
