71f1a176527b0665dbdde069cc7528bce2361657
Yannick <yannick.schrempp@web.de>
2020-10-25 10:53:14 +0100

    Update argparser, black formatting

--- models/model_argparse.py
+++ models/model_argparse.py
@@ -1,29 +1,85 @@
 import argparse
 
-def parse_args(args):
-    """ Parse the arguments.
-    """
 
-    parser = argparse.ArgumentParser(description='Parsing Arguments for training model on video detected data')
-    parser.add_argument('--n-runs',                 help='Number of samples which shall be chosen randomly from hyperparameter serach space', default=50, type=int)
-    parser.add_argument('--topdir-log',             help='The parent directory for logging tensorboard data', default='tensorboard/')
-    parser.add_argument('--top-path',               help='The parent directory where csv feature and label files are stored', default='C:/Users/Yannick/Google Drive/person_detection/pcds_dataset_detections/pcds_dataset_detected_100pcdsimgs_frontinonly/')
-    parser.add_argument('--label-file',             help='The name of the label file', default='pcds_dataset_labels_united.csv')
-    parser.add_argument('--filter-rows-factor',     help="The factor for filtering rows (3 indicates every third row will be left after filtering", type=int)
-    parser.add_argument('--filter-cols-upper',      help='Removes this total amount of columns from the upper boundary of the images',  type=int)
-    parser.add_argument('--filter-cols-lower',      help='Removes this total amount of columns from the lower boundary of the images',  type=int)
-    parser.add_argument('--filter-cols-factor',     help='After removing cols from the ends, filter the csv files with this factor (same as for rows)', type=int)
-    parser.add_argument('--batch-size',             help='The batch size for training', type=int)
-    parser.add_argument('--augmentation-factor',    help='Factor determines how much augmentation will be done [0,1]. If chosen to 1, every detection pixel will be moved randomly 1 step', default=0, type=float)
-    parser.add_argument('--filter-category-noisy',  help='If set to true, category noisy will not be loaded to the dataset', action='store_true', default=False)
-    parser.add_argument('--filter-hour-above',      help='Filter videos which are after this hour during the day due to darkness', type=int, default=0)
-    parser.add_argument('--epochs',                 help='Number of epochs to train', type=int, default=50)
-    parser.add_argument('--warm-start-path',        help='Starting from the best current available snapshot in the provided folder', default='None')
-    parser.add_argument('--schedule-step',          help='The epoch when the learning rate shall be scheduled', type=int, default=0)
-    parser.add_argument('--supercharge-crowdeds',   help='Flag whether files with crowded videos and many persons shall be used more often during training', action='store_true', default=False)
+def parse_args(args):
+    """Parse the arguments."""
+
+    parser = argparse.ArgumentParser(description="Parsing Arguments for training model on video detected data")
+    parser.add_argument(
+        "--n-runs",
+        help="Number of samples which shall be chosen randomly from hyperparameter serach space",
+        default=50,
+        type=int,
+    )
+    parser.add_argument(
+        "--topdir-log", help="The parent directory for logging tensorboard data", default="tensorboard/"
+    )
+    parser.add_argument(
+        "--top-path",
+        help="The parent directory where csv feature and label files are stored",
+        default="C:/Users/Yannick/Google Drive/person_detection/pcds_dataset_detections/pcds_dataset_detected_100pcdsimgs_frontinonly/",
+    )
+    parser.add_argument("--label-file", help="The name of the label file", default="pcds_dataset_labels_united.csv")
+    parser.add_argument(
+        "--filter-rows-factor",
+        help="The factor for filtering rows (3 indicates every third row will be left after filtering",
+        type=int,
+    )
+    parser.add_argument(
+        "--filter-cols-upper",
+        help="Removes this total amount of columns from the upper boundary of the images",
+        type=int,
+    )
+    parser.add_argument(
+        "--filter-cols-lower",
+        help="Removes this total amount of columns from the lower boundary of the images",
+        type=int,
+    )
+    parser.add_argument(
+        "--filter-cols-factor",
+        help="After removing cols from the ends, filter the csv files with this factor (same as for rows)",
+        type=int,
+    )
+    parser.add_argument("--batch-size", help="The batch size for training", type=int)
+    parser.add_argument(
+        "--augmentation-factor",
+        help="Factor determines how much augmentation will be done [0,1]. If chosen to 1, every detection pixel will be moved randomly 1 step",
+        default=0,
+        type=float,
+    )
+    parser.add_argument(
+        "--filter-category-noisy",
+        help="If set to true, category noisy will not be loaded to the dataset",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--filter-hour-above",
+        help="Filter videos which are after this hour during the day due to darkness",
+        type=int,
+        default=0,
+    )
+    parser.add_argument("--epochs", help="Number of epochs to train", type=int, default=50)
+    parser.add_argument(
+        "--warm-start-path",
+        help="Starting from the best current available snapshot in the provided folder",
+        default="None",
+    )
+    parser.add_argument(
+        "--schedule-step", help="The epoch when the learning rate shall be scheduled", type=int, default=0
+    )
+    parser.add_argument(
+        "--supercharge-crowdeds",
+        help="Flag whether files with crowded videos and many persons shall be used more often during training",
+        action="store_true",
+        default=False,
+    )
 
     return parser.parse_args(args)
 
-def check_args(args): 
-    if args.augmentation_factor > 0: 
-        print('Care using Augmentation, significant performance deacreases might be possible depneding on the size of your data')
+
+def check_args(args):
+    if args.augmentation_factor > 0:
+        print(
+            "Care using Augmentation, significant performance deacreases might be possible depneding on the size of your data"
+        )
--- plot_metrics/plot_metrics.py
+++ plot_metrics/plot_metrics.py
@@ -6,27 +6,26 @@ from tensorboard.backend.event_processing.event_accumulator import EventAccumula
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-LOGGING_METRIC = 'epoch_acc_rescaled'
-Y_LABEL = 'Accuracy'
-LOG_DIR_SUP = 'C:/Users/Yannick/Google Drive/person_counting/tensorboard/cnn_regression/cold_start'
-LOG_DIRS = {'best model': 'best' , 
-            'second best model': 'second_best',
-            'third best model': 'third_best'}
+LOGGING_METRIC = "epoch_acc_rescaled"
+Y_LABEL = "Accuracy"
+LOG_DIR_SUP = "C:/Users/Yannick/Google Drive/person_counting/tensorboard/cnn_regression/cold_start"
+LOG_DIRS = {"best model": "best", "second best model": "second_best", "third best model": "third_best"}
 SMOOTH_NUM = 1
 
-plt.style.use('ggplot')
+plt.style.use("ggplot")
+
 
 def plot_tensorflow_log(log_paths):
-    figure = plt.figure(figsize=(8,8))
-    for net, path in log_paths.items():        
+    figure = plt.figure(figsize=(8, 8))
+    for net, path in log_paths.items():
 
         event_acc = EventAccumulator(path)
         event_acc.Reload()
-        print('Available metrics: ', event_acc.Tags())
+        print("Available metrics: ", event_acc.Tags())
         parsed_metrics = list()
-        for metric in event_acc.Tags()['scalars']: 
+        for metric in event_acc.Tags()["scalars"]:
 
-            if (metric in LOGGING_METRIC) and (metric not in parsed_metrics):               
+            if (metric in LOGGING_METRIC) and (metric not in parsed_metrics):
                 metric_values = event_acc.Scalars(metric)
 
                 steps = len(metric_values)
@@ -35,51 +34,54 @@ def plot_tensorflow_log(log_paths):
                 y_smooth = np.zeros([steps, 2])
 
                 for i in range(steps):
-                    y[i] = metric_values[i][2] # value
+                    y[i] = metric_values[i][2]  # value
 
-                    if i > SMOOTH_NUM: 
-                        y_smooth[i] = np.average(y[i-SMOOTH_NUM:i])
-                    else: 
+                    if i > SMOOTH_NUM:
+                        y_smooth[i] = np.average(y[i - SMOOTH_NUM : i])
+                    else:
                         y_smooth[i] = np.average(y[0:i])
 
-                if ('validation' in path) or ('mAP' in metric): 
-                    #validation folder is the last which is parsed so show the plot and save it 
+                if ("validation" in path) or ("mAP" in metric):
+                    # validation folder is the last which is parsed so show the plot and save it
                     plt.plot(x, y_smooth[:], label=f'validation Accuracy for {net[:net.find("validation")]}')
                     parsed_metrics.append(metric)
-
 
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys())
     plt.ylabel(Y_LABEL)
-    plt.xlabel('epoch')
+    plt.xlabel("epoch")
     plt.show()
-    #figure.savefig(f'./plot_metrics/pcds_bestvalidations_{LOGGING_METRIC}.png', type='png', dpi=2400)
+    # figure.savefig(f'./plot_metrics/pcds_bestvalidations_{LOGGING_METRIC}.png', type='png', dpi=2400)
     mpl.use("pgf")
-    mpl.rcParams.update({
-        "pgf.texsystem": "pdflatex",
-        'font.family': 'serif',
-        'text.usetex': True,
-        'pgf.rcfonts': False,
-    })
-    figure.savefig(f'./plot_metrics/pcds_bestvalidations_{LOGGING_METRIC}.pgf', type='pgf')
+    mpl.rcParams.update(
+        {
+            "pgf.texsystem": "pdflatex",
+            "font.family": "serif",
+            "text.usetex": True,
+            "pgf.rcfonts": False,
+        }
+    )
+    figure.savefig(f"./plot_metrics/pcds_bestvalidations_{LOGGING_METRIC}.pgf", type="pgf")
+
 
 def get_log_paths():
     log_paths = dict()
 
     for net, log_dir in LOG_DIRS.items():
         for root, _, files in os.walk(os.path.join(LOG_DIR_SUP, log_dir)):
-            for file_name in files: 
-                if file_name[-3:] == '.v2':
+            for file_name in files:
+                if file_name[-3:] == ".v2":
 
-                    if 'validation' in root:
-                        log_paths[net + ' validation'] = os.path.join(root, file_name)
-                    elif not 'train' in root: 
-                        log_paths[net + ' train_val'] = os.path.join(root, file_name)
-                    else: 
-                        log_paths[net + ' train'] = os.path.join(root, file_name)
+                    if "validation" in root:
+                        log_paths[net + " validation"] = os.path.join(root, file_name)
+                    elif not "train" in root:
+                        log_paths[net + " train_val"] = os.path.join(root, file_name)
+                    else:
+                        log_paths[net + " train"] = os.path.join(root, file_name)
     return log_paths
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     log_paths = get_log_paths()
     plot_tensorflow_log(log_paths)
